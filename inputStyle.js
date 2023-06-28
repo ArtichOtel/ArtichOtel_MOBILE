28e854df4da0095ef4997c52d6171b8471c55971
butch <au.bouchard@gmail.com>
2023-06-28 10:33:34 +0200

    :sparkles: connection view WIP

---
+++ inputStyle.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from "react-native";
+
+const inputStyle = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        padding: 10,
+        borderRadius: 10
+    }
+})
+
+
+export default inputStyle;
---
+++ views/ConnectionView.tsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+import {View} from "react-native";
+
+type ConnectionProps = {
+    placeholder: string;
+}
+
+
+export function ConnectionView(props: ConnectionProps): JSX.Element {
+    const {placeholder} = props
+
+    return (
+        <View style={}
+    )
+}
