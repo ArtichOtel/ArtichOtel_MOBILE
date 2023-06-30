49f320bfb7051dc9a0693682705dc3f27cdc0739
Mathieu <berthetmathieu@free.fr>
2023-06-30 10:07:03 +0200

    background green

--- views/PresentChamberView.tsx
+++ views/PresentChamberView.tsx
@@ -12,7 +12,7 @@ export default function PresentChamberView(props: any): JSX.Element {
       <View 
       style={baseStyle.container}
       >
-            <View style={[baseStyle.container, presentChamberStyle.infoBox]}>
+            <View style={[baseStyle.container, presentChamberStyle.infoBox, presentChamberStyle.contentCenter]}>
                 <Text>X personnes  -  </Text>
                 <Text>29/06/2023  -  </Text>
                 <Text>30/06/2023</Text>
@@ -24,36 +24,36 @@ export default function PresentChamberView(props: any): JSX.Element {
 
                 <Text style={baseStyle.textTypo}>Voici une chambre Standard pour maximum 3 personnes, avec des lits douillés, ainsi qu'un confort inégalable. Télé, service de chambre, douche et autres avantages vous attendent</Text>
 
-                <View style={presentChamberStyle.arrivalHourContainer}>
+                <View style={[presentChamberStyle.arrivalHourContainer, presentChamberStyle.contentCenter]}>
                     <FontAwesomeIcon icon={faClock} size={40} style={{marginRight: 15}} />
                     <Text style={baseStyle.textTypo}> Arrivée entre 13h00 et 18h00</Text>
                 </View>
 
-                <View style={presentChamberStyle.greatContainer}>
-                    <View style={presentChamberStyle.middleContainer}>
-                        <View style={presentChamberStyle.littleContainer}>
+                <View style={[presentChamberStyle.advantagesContainer, presentChamberStyle.contentCenter]}>
+                    <View style={[presentChamberStyle.columnContainer, presentChamberStyle.contentCenter]}>
+                        <View style={[presentChamberStyle.itemContainer, presentChamberStyle.contentCenter]}>
                             <FontAwesomeIcon icon={faShower} size={40} style={{marginRight: 15}}/>
                             <Text style={baseStyle.textTypo}>Douche</Text>
                         </View>
-                        <View style={presentChamberStyle.littleContainer} >
+                        <View style={[presentChamberStyle.itemContainer, presentChamberStyle.contentCenter]} >
                             <FontAwesomeIcon icon={faBed} size={40} style={{marginRight: 15}}/>
                             <Text style={baseStyle.textTypo} >Grand lit</Text>
                         </View>
-                        <View style={presentChamberStyle.littleContainer} >
+                        <View style={[presentChamberStyle.itemContainer, presentChamberStyle.contentCenter]} >
                             <FontAwesomeIcon icon={faSmokingBan} size={40} style={{marginRight: 10}}/>
                             <Text style={baseStyle.textTypo} >Ne pas fumer</Text>
                         </View>
                     </View>
-                    <View style={presentChamberStyle.middleContainer}>
-                        <View style={presentChamberStyle.littleContainer} >
+                    <View style={[presentChamberStyle.columnContainer, presentChamberStyle.contentCenter]}>
+                        <View style={presentChamberStyle.itemContainer} >
                             <FontAwesomeIcon icon={faTelevision} size={40} style={{marginRight: 15}}/>
                             <Text style={baseStyle.textTypo} >Télévision</Text>
                         </View>
-                        <View style={presentChamberStyle.littleContainer} >
+                        <View style={[presentChamberStyle.itemContainer, presentChamberStyle.contentCenter]} >
                             <FontAwesomeIcon icon={faKitchenSet} size={40} style={{marginRight: 15}} />
                             <Text style={baseStyle.textTypo} >Frigo et Cuisine</Text>
                         </View>
-                        <View style={presentChamberStyle.littleContainer} >
+                        <View style={[presentChamberStyle.itemContainer, presentChamberStyle.contentCenter]} >
                             <FontAwesomeIcon icon={faBellConcierge} size={40} style={{marginRight: 5}}/>
                             <Text style={baseStyle.textTypo} >Service Chambre</Text>
                         </View>
@@ -63,9 +63,9 @@ export default function PresentChamberView(props: any): JSX.Element {
 
         
 
-        <View style={presentChamberStyle.littleContainer}>
-            <TouchableOpacity style={presentChamberStyle.buttonPrice}><Text style={presentChamberStyle.buttonTextColor}>200€</Text></TouchableOpacity>
-            <TouchableOpacity style={presentChamberStyle.buttonValid}><Text style={presentChamberStyle.buttonTextColor}>Selectionner</Text></TouchableOpacity>
+        <View style={[presentChamberStyle.buttonBackgroundContainer, presentChamberStyle.contentCenter]}>
+            <TouchableOpacity style={[presentChamberStyle.buttonPrice, presentChamberStyle.contentCenter]}><Text style={presentChamberStyle.buttonTextColor}>200€</Text></TouchableOpacity>
+            <TouchableOpacity style={[presentChamberStyle.buttonValid, presentChamberStyle.contentCenter]}><Text style={presentChamberStyle.buttonTextColor}>Selectionner</Text></TouchableOpacity>
         </View>
       </View>
     );
