a296e6b0b923ead6ed8624b5fc63b189d5f2e2c8
Anthony26100 <anthony.bellier@laposte.net>
2023-07-27 15:02:27 +0200

    ✨ Navigation flow for signup

--- utils/profileUpdater.tsx
+++ utils/profileUpdater.tsx
@@ -1,40 +1,40 @@
 import axios from "axios";
 //@ts-ignore
-import {API_URL} from '@env'
-import {credentials} from "./types";
-
+import { API_URL } from "@env";
+import { credentials } from "./types";
 
 // from log in => get data
-export async function getUserData({cred}): Promise<any> {
-    console.log("get user data", cred.id, cred.token)
-    return axios.get(API_URL+"user/"+cred.id, {
-        headers:{
-            'Authorization': `Bearer ${cred.token}`
-        }
-    }).then((resp) => {
-        console.log("-------------------------------------")
-        console.log("user:", resp.data)
-        console.log("-------------------------------------")
-        return {data: resp.data, cred: cred}
+export async function getUserData({ cred }): Promise<any> {
+  console.log("get user data", cred.user_id, cred.token);
+  return axios
+    .get(API_URL + "user/" + cred.user_id, {
+      headers: {
+        Authorization: `Bearer ${cred.token}`,
+      },
     })
-        .catch(err => console.log("err", err))
+    .then((resp) => {
+      //   console.log("-------------------------------------");
+      //   console.log("user:", resp.data);
+      //   console.log("-------------------------------------");
+      return { data: resp.data, cred: cred };
+    })
+    .catch((err) => console.log("err", err));
 }
-
-
 
 // from log in if is customer=> get more data
-export async function getCustomerData({cred}): Promise<any> {
-    console.log("get customer data", cred.customer, cred.token)
-    return axios.get(API_URL+"customer/"+cred.customer, {
-        headers:{
-            'Authorization': `Bearer ${cred.token}`
-        }
-    }).then((resp) => {
-        console.log("-------------------------------------")
-        console.log("customer:", resp.data)
-        console.log("-------------------------------------")
-        return {data: resp.data, cred: cred}
+export async function getCustomerData({ cred }): Promise<any> {
+  console.log("get customer data", cred.customer, cred.token);
+  return axios
+    .get(API_URL + "customer/" + cred.customer, {
+      headers: {
+        Authorization: `Bearer ${cred.token}`,
+      },
     })
-        .catch(err => console.log("err", err))
+    .then((resp) => {
+      console.log("-------------------------------------");
+      console.log("customer:", resp.data);
+      console.log("-------------------------------------");
+      return { data: resp.data, cred: cred };
+    })
+    .catch((err) => console.log("err", err));
 }
-
--- views/SignUpView.tsx
+++ views/SignUpView.tsx
@@ -1,224 +1,253 @@
-import React, {useContext, useState} from "react";
-import {Animated, Text, TextInput, TouchableOpacity, View} from "react-native";
+import React, { useContext, useState } from "react";
+import {
+  Animated,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import connectionStyle from "../style/ConnectionStyle";
 import inputStyle from "../style/inputStyle";
 import MainMenu from "../components/tabs/MainMenu";
 import baseStyle from "../style/baseStyle";
 import buttonStyle from "../style/buttonStyle";
-import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
-import { faUserPlus} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import mainMenuStyle from "../style/mainMenuStyle";
 import axios from "axios";
 // @ts-ignore
-import {API_URL} from "@env";
-import {userDataType, userProfileType} from "../utils/types";
-import {UserCtx, UserProfileCtx} from "../utils/context";
+import { API_URL } from "@env";
+import { userDataType, userProfileType } from "../utils/types";
+import { UserCtx, UserProfileCtx } from "../utils/context";
 import ScrollView = Animated.ScrollView;
 import SignUpViewStyle from "../style/SignUpViewStyle";
-import {getUserData} from "../utils/profileUpdater";
-
+import { getUserData } from "../utils/profileUpdater";
 
 type ConnectionProps = {
-    navigation: any;
-}
+  navigation: any;
+  route: any;
+};
 
+function ConnectionView(props: ConnectionProps): JSX.Element | null {
+  const { navigation, route } = props;
+  const { currentUser, setCurrentUser } = useContext(UserCtx);
+  const { userProfile, setUserProfile } = useContext(UserProfileCtx);
+  const [lastname, setLastname] = useState<string>("");
+  const [firstname, setFirstname] = useState<string>("");
+  const [pseudo, setPseudo] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [connectionError, setConnectionError] = useState<string | null>(null);
 
-function ConnectionView(props: ConnectionProps): JSX.Element|null {
-    const {navigation} = props
-    const {currentUser, setCurrentUser} = useContext(UserCtx)
-    const {userProfile, setUserProfile} = useContext(UserProfileCtx)
-    const [lastname, setLastname] = useState<string>('')
-    const [firstname, setFirstname] = useState<string>('')
-    const [pseudo, setPseudo] = useState<string>('')
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [connectionError, setConnectionError] = useState<string|null>(null)
+  const searchReservationsResult: object = route.params
+    ?.searchReservationsResult
+    ? route.params.searchReservationsResult
+    : null;
+  const nextScreen: object = route.params?.nextScreen
+    ? route.params.nextScreen
+    : null;
+  //   console.log("route.params in Signupview", route.params);
+  //   console.log(
+  //     "searchReservationsResult in Signupview",
+  //     searchReservationsResult
+  //   );
 
-
-    const postCreateUser = () => {
-        let bodyJSON = {
-            last_name: lastname,
-            first_name: firstname,
-            pseudo: pseudo,
-            email: email,
-            password: password,
-            lang: 'fr_FR'
-    }
-        return axios
-            .post(API_URL+"user/register",
-                bodyJSON)
-            .then((response) => {
-                console.log("RESPONSE",response.data)
-                alert(`Bienvenue ${response.data[0].pseudo} !`)
-                return response.data;
-            })
-            .catch((err) => {
-                console.log("connection error :", err);
-                setConnectionError(err.response.data.message)
-            });
+  const postCreateUser = () => {
+    let bodyJSON = {
+      last_name: lastname,
+      first_name: firstname,
+      pseudo: pseudo,
+      email: email,
+      password: password,
+      lang: "fr_FR",
     };
+    return axios
+      .post(API_URL + "user/register", bodyJSON)
+      .then((response) => {
+        console.log("RESPONSE", response.data);
+        alert(`Bienvenue ${response.data[0].pseudo} !`);
+        return response.data;
+      })
+      .catch((err) => {
+        console.log("connection error :", err);
+        setConnectionError(err.response.data.message);
+      });
+  };
 
+  function autoLogin(pseudo: string) {
+    return axios
+      .post(API_URL + "user/login", {
+        pseudo: pseudo,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        const user: userDataType = {
+          userId: response.data.user_id,
+          token: response.data.token,
+          customerId: response.data.customer_id,
+        };
+        setCurrentUser(user);
+        return response.data;
+      })
+      .catch((err) => {
+        console.log("signup error :", err);
+        setConnectionError(err.response.data.message);
+      });
+  }
 
-    function autoLogin(pseudo:string) {
-        return axios.post(API_URL+"user/login", {
-            pseudo: pseudo,
-            email: email,
-            password: password
-        })
-            .then((response) => {
-                const user: userDataType = {
-                    userId: response.data.user_id,
-                    token: response.data.token,
-                    customerId: response.data.customer_id
-                }
-                setCurrentUser(user)
-                return response.data
+  function trySignUp(): void {
+    postCreateUser()
+      .then((userData) => {
+        return autoLogin(userData[0].pseudo);
+      })
+      .then((cred) => {
+        console.log("signup success", cred);
+        return getUserData({ cred });
+      })
+      .then((data: any) => {
+        //console.log("conection success, now try update user profile ctx", data);
+
+        const userData: userProfileType = {
+          dateCreated: data.created_at,
+          email: data.email,
+          pseudo: data.pseudo,
+          dateUpdate: data.updated_at,
+          //firstName: data.,
+          //lastName: data.
+        };
+
+        setUserProfile(userData);
+      })
+      .then(() => {
+        // TODO : change destination according to global state next view
+        console.log("route.params in Signupview", nextScreen);
+        // console.log(
+        //   "searchReservationsResult in Signupview",
+        //   searchReservationsResult
+        // );
+        nextScreen
+          ? navigation.navigate(route.params.nextScreen, {
+              searchReservationsResult: searchReservationsResult,
             })
-            .catch((err) => {
-                console.log("signup error :", err);
-                setConnectionError(err.response.data.message)
-            });
-    }
+          : navigation.navigate("Main");
+      })
+      .catch((err) => {
+        console.log("error in signupview", err);
+      });
+  }
 
-    function trySignUp():void {
-        postCreateUser()
-            .then((userData) => {
-                return autoLogin(userData[0].pseudo)
-            })
-            .then((cred)=> {
-                console.log("signup success", cred)
-                return getUserData(cred.user_id, cred.token)
-            })
-            .then((data: any)=>{
-                console.log("conection success, now try update user profile ctx", data)
+  return !currentUser ? null : (
+    <View style={baseStyle.view}>
+      <ScrollView style={[SignUpViewStyle.container]}>
+        <View style={connectionStyle.container}>
+          <View style={[inputStyle.labelWrapper, connectionStyle.first]}>
+            <Text style={[inputStyle.label]}>Nom</Text>
+            <Text style={[inputStyle.needed]}>*</Text>
+          </View>
 
-                const userData: userProfileType = {
-                    dateCreated: data.created_at,
-                    email: data.email,
-                    pseudo: data.pseudo,
-                    dateUpdate: data.updated_at
-                    //firstName: data.,
-                    //lastName: data.
-                }
+          <TextInput
+            style={[baseStyle.input, connectionStyle.input]}
+            autoComplete={"name-family"}
+            blurOnSubmit={true}
+            inputMode="text"
+            onChangeText={(val) => setLastname(val)}
+            onPressIn={() => setConnectionError(null)}
+            placeholder={"nom"}
+            value={lastname}
+          />
 
-                setUserProfile(userData)
-            })
-            .then(() => {
-                // TODO : change destination according to global state next view
-                navigation.navigate('Main')
-            })
-    }
+          <View style={inputStyle.labelWrapper}>
+            <Text style={[inputStyle.label]}>Prénom</Text>
+            <Text style={[inputStyle.needed]}>*</Text>
+          </View>
 
+          <TextInput
+            style={[baseStyle.input, connectionStyle.input]}
+            autoComplete={"name"}
+            blurOnSubmit={true}
+            inputMode="text"
+            onChangeText={(val) => setFirstname(val)}
+            onPressIn={() => setConnectionError(null)}
+            placeholder={"prénom"}
+            value={firstname}
+          />
 
-    return (
-        !currentUser ? null :
-            <View style={baseStyle.view}>
-                <ScrollView
-                    style={[SignUpViewStyle.container]}
-                >
-                    <View style={connectionStyle.container}>
+          <View style={inputStyle.labelWrapper}>
+            <Text style={[inputStyle.label]}>Pseudo</Text>
+          </View>
 
-                        <View style={[inputStyle.labelWrapper, connectionStyle.first]}>
-                            <Text style={[inputStyle.label]}>Nom</Text>
-                            <Text style={[inputStyle.needed]}>*</Text>
-                        </View>
+          <TextInput
+            style={[baseStyle.input, connectionStyle.input]}
+            autoComplete={"username"}
+            blurOnSubmit={true}
+            inputMode="text"
+            onChangeText={(val) => setPseudo(val)}
+            onPressIn={() => setConnectionError(null)}
+            placeholder={"pseudo"}
+            value={pseudo}
+          />
 
-                        <TextInput
-                            style={[baseStyle.input, connectionStyle.input]}
-                            autoComplete={"name-family"}
-                            blurOnSubmit={true}
-                            inputMode="text"
-                            onChangeText={(val) => setLastname(val)}
-                            onPressIn={() => setConnectionError(null)}
-                            placeholder={"nom"}
-                            value={lastname}
-                        />
+          <View style={inputStyle.labelWrapper}>
+            <Text style={[inputStyle.label]}>Email</Text>
+            <Text style={[inputStyle.needed]}>*</Text>
+          </View>
 
-                        <View style={inputStyle.labelWrapper}>
-                            <Text style={[inputStyle.label]}>Prénom</Text>
-                            <Text style={[inputStyle.needed]}>*</Text>
-                        </View>
+          <TextInput
+            style={[baseStyle.input, connectionStyle.input]}
+            autoComplete={"email"}
+            blurOnSubmit={true}
+            inputMode="text"
+            onChangeText={(val) => setEmail(val)}
+            onPressIn={() => setConnectionError(null)}
+            placeholder={"identifiant"}
+            value={email}
+          />
 
-                        <TextInput
-                            style={[baseStyle.input, connectionStyle.input]}
-                            autoComplete={"name"}
-                            blurOnSubmit={true}
-                            inputMode="text"
-                            onChangeText={(val) => setFirstname(val)}
-                            onPressIn={() => setConnectionError(null)}
-                            placeholder={"prénom"}
-                            value={firstname}
-                        />
+          <View style={inputStyle.labelWrapper}>
+            <Text style={[inputStyle.label]}>Mot de passe</Text>
+            <Text style={[inputStyle.needed]}>*</Text>
+          </View>
 
-                        <View style={inputStyle.labelWrapper}>
-                            <Text style={[inputStyle.label]}>Pseudo</Text>
-                        </View>
+          <TextInput
+            style={[baseStyle.input, connectionStyle.input]}
+            autoComplete={"username"}
+            blurOnSubmit={true}
+            inputMode="text"
+            secureTextEntry={true}
+            onChangeText={(val) => setPassword(val)}
+            onPressIn={() => setConnectionError(null)}
+            placeholder={"mot de passe"}
+            value={password}
+          />
 
-                        <TextInput
-                            style={[baseStyle.input, connectionStyle.input]}
-                            autoComplete={"username"}
-                            blurOnSubmit={true}
-                            inputMode="text"
-                            onChangeText={(val) => setPseudo(val)}
-                            onPressIn={() => setConnectionError(null)}
-                            placeholder={"pseudo"}
-                            value={pseudo}
-                        />
+          <View style={[connectionStyle.buttonWrapper]}>
+            <TouchableOpacity
+              style={[baseStyle.btn, buttonStyle.dark]}
+              onPress={() => trySignUp()}
+            >
+              <FontAwesomeIcon
+                icon={faUserPlus}
+                size={30}
+                style={mainMenuStyle.items}
+              />
+              <Text
+                style={[
+                  baseStyle.textTypo,
+                  baseStyle.textLight,
+                  connectionStyle.button,
+                ]}
+              >
+                S'inscrire
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
 
-                        <View style={inputStyle.labelWrapper}>
-                            <Text style={[inputStyle.label]}>Email</Text>
-                            <Text style={[inputStyle.needed]}>*</Text>
-                        </View>
-
-                        <TextInput
-                            style={[baseStyle.input, connectionStyle.input]}
-                            autoComplete={"email"}
-                            blurOnSubmit={true}
-                            inputMode="text"
-                            onChangeText={(val) => setEmail(val)}
-                            onPressIn={() => setConnectionError(null)}
-                            placeholder={"identifiant"}
-                            value={email}
-                        />
-
-
-                        <View style={inputStyle.labelWrapper}>
-                            <Text style={[inputStyle.label]}>Mot de passe</Text>
-                            <Text style={[inputStyle.needed]}>*</Text>
-                        </View>
-
-                        <TextInput
-                            style={[baseStyle.input, connectionStyle.input]}
-                            autoComplete={"username"}
-                            blurOnSubmit={true}
-                            inputMode="text"
-                            secureTextEntry={true}
-                            onChangeText={(val) => setPassword(val)}
-                            onPressIn={() => setConnectionError(null)}
-                            placeholder={"mot de passe"}
-                            value={password}
-                        />
-
-                        <View style={[connectionStyle.buttonWrapper]}
-                        >
-
-                            <TouchableOpacity style={[baseStyle.btn, buttonStyle.dark]}
-                                              onPress={() => trySignUp()}
-                            >
-                                <FontAwesomeIcon icon={faUserPlus} size={30} style={mainMenuStyle.items} />
-                                <Text style={[baseStyle.textTypo, baseStyle.textLight, connectionStyle.button]}>S'inscrire</Text>
-                            </TouchableOpacity>
-
-
-                        </View>
-                    </View>
-
-
-                </ScrollView>
-
-                <MainMenu navigation={navigation}  />
-            </View>
-    )
+      <MainMenu navigation={navigation} />
+    </View>
+  );
 }
 
 export default ConnectionView;
